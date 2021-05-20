26219575e72304bdda3226c1fe9ed52291441761
Yukidama <narururu19@gmail.com>
2021-05-20 15:41:52 +0900

    initial commit

---
+++ narutils/__init__.py
@@ -0,0 +1,1 @@
+from narutils.narutils import *
---
+++ narutils/narutils.py
@@ -0,0 +1,20 @@
+def decopri(txt):
+    print("-"*80)
+    if len(txt)<80:
+        print(" "*int((80-len(txt))/2)+str(txt)+" "*int((80-len(txt))/2))
+    else:
+        print(str(txt))
+    print("-" * 80)
+
+def decopri_title(title,txt):
+    # print("-"*80)
+    # if len(title)<80:
+    #     print(" "*int((80-len(title))/2)+str(title)+" "*int((80-len(title))/2))
+    # else:
+    #     print(str(title))
+    txt=title+" :"+str(txt)
+    if len(txt)<80:
+        print(" "*int((80-len(txt))/2)+str(txt)+" "*int((80-len(txt))/2))
+    else:
+        print(str(txt))
+    print("-" * 80)
