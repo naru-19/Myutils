9ddd333d648297500c41610066b5f3a9f56ff3ee
naru-19 <61283753+naru-19@users.noreply.github.com>
2021-05-21 14:26:42 +0900

    add decostar

--- narutils/narutils.py
+++ narutils/narutils.py
@@ -18,3 +18,29 @@ def decopri_title(title,txt):
     else:
         print(str(txt))
     print("-" * 80)
+
+    
+    
+def decost(txt):
+    print("☆"*80)
+    if len(str(txt))<80:
+        print(" "*int((80-len(str(txt)))/2)+str(txt)+" "*int((80-len(str(txt)))/2))
+    else:
+        print(str(txt))
+    print("☆" * 80)
+    
+def decopri_title(title,txt):
+    # print("-"*80)
+    # if len(title)<80:
+    #     print(" "*int((80-len(title))/2)+str(title)+" "*int((80-len(title))/2))
+    # else:
+    #     print(str(title))
+    txt=title+" :"+str(txt)
+    if len(str(txt))<80:
+        print(" "*int((80-len(str(txt)))/2)+str(txt)+" "*int((80-len(str(txt)))/2))
+    else:
+        print(str(txt))
+    print("☆" * 80)
+    
+    
+    
