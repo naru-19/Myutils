10768c4d7d1b2eddfaeec98a009aa993e89e58fa
naru-19 <61283753+naru-19@users.noreply.github.com>
2021-05-23 10:01:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 from pip.req import parse_requirements
 import sys, os, pip
 
-requirements = [
-    str(requirement.req)
-    for requirement in parse_requirements('requirements.txt', session = pip.download.PipSession())
-]
+# requirements = [
+#     str(requirement.req)
+#     for requirement in parse_requirements('requirements.txt', session = pip.download.PipSession())
+# ]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -25,6 +25,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
+#     install_requires=requirements,
     python_requires='>=2.7',
 )
